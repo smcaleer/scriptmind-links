76154ec441b17cebb16a04c4c30f42ed5649a5d0
aotearoanz <aotearoanz@gmail.com>
2012-05-17 11:59:13 +0000

    Update logo image, fix syntax error

--- include/rss_parser.php
+++ include/rss_parser.php
@@ -11,15 +11,15 @@
  ***************************************************************************/
 
 // This rssParser will take a url to an RSS feed and instantiate an xml parser
-// to read it. The channel details will be stored in three variable, $channel, 
-// $channel_description & $channel_link. The news items from the rss feed will 
+// to read it. The channel details will be stored in three variable, $channel,
+// $channel_description & $channel_link. The news items from the rss feed will
 // be stored in three arrays $titles, $descriptions & $liks.
 
-class rssParser 
+class rssParser
 {
 	// private - an _ (underscore) will hide these fields from public view
 	//					effectivly making them private fields.
-	
+
 	var $_insideitem = false;
 	var $_insidechannel = false;
 	var $_insideimage = false;
@@ -46,26 +46,26 @@ class rssParser
 	var $image_width='0';
 	var $image_height='0';
 	var $image_title='';
-    
+
 	var $channel='';
 	var $channel_description='';
 	var $channel_link='';
-	
+
     var $items = array();
-	
+
 	var $error_msg='';
 
-	
+
 	// our class constructor
 	function rssParser()
 	{
 		// instantiate an xml parser and setup the resuired events
 		$this->_xml_parser = xml_parser_create();
-		xml_set_object($this->_xml_parser,&$this);
+		xml_set_object($this->_xml_parser,$this);
 		xml_set_element_handler($this->_xml_parser, 'startElement', 'endElement');
 		xml_set_character_data_handler($this->_xml_parser, 'characterData');
 	}
-	
+
 	// our class destructor (kind of)
 	function destroy()
 	{
@@ -74,20 +74,20 @@ class rssParser
 		{
 			fclose($this->_fp);
 		}
-		
+
 		// destroy our xml parser
 		xml_parser_free($this->_xml_parser);
 	}
-	
+
 	// parse the xml
 	function parse($RSS_URL)
 	{
 		$prev_error_reporting = error_reporting();
 		error_reporting(E_ERROR);
-		
+
 		$result = false;
 		$data = '';
-		
+
 		// retrieve the data from the given url
 		if ($this->_fp = @fopen($RSS_URL,'r'))
 		{
@@ -96,55 +96,55 @@ class rssParser
 			{
 				// parse our xml data until we find the end of our file
 				if(!xml_parse($this->_xml_parser, $data, feof($this->_fp)))
-					return sprintf('XML error: %s at line %d', 
-					xml_error_string(xml_get_error_code($this->_xml_parser)), 
+					return sprintf('XML error: %s at line %d',
+					xml_error_string(xml_get_error_code($this->_xml_parser)),
 					xml_get_current_line_number($this->_xml_parser));
 
 				$result = true;
-			}	
+			}
 		}
 		error_reporting($prev_error_reporting);
 
-		return $result;	
+		return $result;
 	}
-	
+
 	// this event is fired when the xml parser comes across and opening element in the xml
-	function startElement($parser, $tagName, $attrs) 
+	function startElement($parser, $tagName, $attrs)
 	{
-		// if we are already inside an item then	
-		if ($this->_insideitem) 
+		// if we are already inside an item then
+		if ($this->_insideitem)
 		{
 			// take a copy of the tag name
 			$this->_tag = $tagName;
-			
-			// set insidechannel to false - this is done to get around the problem of 
+
+			// set insidechannel to false - this is done to get around the problem of
 			// some rss feeds wrapping their news items inside a channel while others
 			// just declare the channel on it's own at the top of the rss feed.
 			$this->_insidechannel = false;
-		} 
-		
+		}
+
 		// if we are on an ITEM tag
-		elseif ($tagName == 'ITEM') 
+		elseif ($tagName == 'ITEM')
 		{
 			// set insideitem to true
 			$this->_insideitem = true;
 		}
-		
+
 		// if we are on a CHANNEL tag
 		elseif ($tagName =='CHANNEL')
 		{
 			// set insidechannel to true
 			$this->_insidechannel = true;
 		}
-		
+
 		// if we are on an IMAGE tag
-		elseif ($tagName == 'IMAGE') 
+		elseif ($tagName == 'IMAGE')
 		{
 			$this->_insideimage = true;
 			$this->_insidechannel = false;
 			$this->_insideitem = false;
 		}
-		
+
 		// if we are inside an image
 		elseif ($this->_insideimage)
 		{
@@ -152,7 +152,7 @@ class rssParser
 			$this->_insidechannel = false;
 		}
 
-		// if we are inside a channel - do this bit last just in case the channel tag 
+		// if we are inside a channel - do this bit last just in case the channel tag
 		// wraps the whole news feed, then we'd just be inside a channel all the time
 		elseif ($this->_insidechannel)
 		{
@@ -161,10 +161,10 @@ class rssParser
 	}
 
 	// this event is fired when the xml parser comes across a closing element in the xml
-	function endElement($parser, $tagName) 
+	function endElement($parser, $tagName)
 	{
 		// if it's an item then we want to add the item details to our item details arrays
-		if ($tagName == 'ITEM') 
+		if ($tagName == 'ITEM')
 		{
             $this->_item['title'] = trim($this->_item['title']);
             $this->_item['description'] = trim($this->_item['description']);
@@ -175,7 +175,7 @@ class rssParser
             $this->_item = array();
 			$this->_insideitem = false;
 		}
-		
+
 		// if it's an image
 		elseif ($tagName == 'IMAGE')
         {
@@ -210,16 +210,16 @@ class rssParser
 
 	// this event is fired for each character of data which is read from the xml
 	// use it to read the bits of information we are interested in
-	function characterData($parser, $data) 
+	function characterData($parser, $data)
 	{
 		// if we are inside an item
-		if ($this->_insideitem) 
+		if ($this->_insideitem)
 		{
 			// depending on what tag we are looking at
 			// set the appropriate bit variable - notice that the data
-			// is added on to the end of the variable, this is because 
+			// is added on to the end of the variable, this is because
 			// it's read one bit at a time.
-			switch ($this->_tag) 
+			switch ($this->_tag)
 			{
 				case 'TITLE':
 					$this->_item['title'] .= $data;
@@ -232,15 +232,15 @@ class rssParser
 					break;
 			}
 		}
-		
+
         // if was inside an image
         elseif ($this->_insideimage)
         {
 			// depending on what tag we are looking at
 			// set the appropriate bit variable - notice that the data
-			// is added on to the end of the variable, this is because 
+			// is added on to the end of the variable, this is because
 			// it's read one bit at a time.
-			switch ($this->_tag) 
+			switch ($this->_tag)
 			{
 				case 'URL':
 					$this->_image_url .= $data;
@@ -265,9 +265,9 @@ class rssParser
 		{
 			// depending on what tag we are looking at
 			// set the appropriate bit variable - notice that the data
-			// is added on to the end of the variable, this is because 
+			// is added on to the end of the variable, this is because
 			// it's read one bit at a time.
-			switch ($this->_tag) 
+			switch ($this->_tag)
 			{
 				case 'TITLE':
 					$this->_channel .= $data;
